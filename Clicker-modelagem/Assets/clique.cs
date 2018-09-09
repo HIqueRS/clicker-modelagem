7e10fe17bf4c3dbd3d49b35cdb5f4c2732ab7712
HIqueRS <39134729+HIqueRS@users.noreply.github.com>
2018-09-09 16:24:54 -0300

    mecanica base

---
+++ Clicker-modelagem/Assets/clique.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class clique : MonoBehaviour {
+
+    public Text score;
+    public int Nresources =0;
+    public int resourcesPerClick = 1;
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        score.text = "recursos: "+ Nresources;
+		
+	}
+
+    public void Cliked()
+    {
+        Nresources += resourcesPerClick;
+    }
+}
